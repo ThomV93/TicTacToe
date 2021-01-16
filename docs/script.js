ade97dda1cd76726ba4a30bdbda7180e75be7115
Thomas Veit <thomas.veit93@gmail.com>
2021-01-16 20:09:30 +0000

    player can select marker/ comments added

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,9 @@
 const player = (marker) => {
     let _marker = marker;
     let getMarker = () => _marker;
+    let setMarker = (newMarker) => _marker = newMarker; 
 
-    return {getMarker};
+    return {getMarker, setMarker};
 };
 
 //game module
@@ -11,10 +12,11 @@ const game = (() => {
 
     //check for winner
     let checkWin = ((idx) => {
+        //the combinations with 4 digits cover the diagonal exceptions
         let _possibleWins = ["048", "246", "012", "345", "678", "036", "147", "258", "0148", "0248", "0348", "0458", "0468", "0478", "2346", "2456"]; 
         //RegEx constructor from all possible winning combinations
-        let masterPattern = new RegExp(_possibleWins.join("|"));
-        return masterPattern.test(idx);
+        let _masterPattern = new RegExp(_possibleWins.join("|"));
+        return _masterPattern.test(idx);
     });
 
     return {checkWin};
@@ -27,6 +29,10 @@ const board = (() => {
 
     //cache all DOM elements
     const _displayBoard = Array.from(document.getElementsByClassName("board-square"));
+    const _xbuttonMarker = document.getElementById("btn-x");
+    const _obuttonMarker = document.getElementById("btn-o");
+    const _playerScore = document.getElementById("player-score");
+    const _computerScore = document.getElementById("computer-score");
 
     //sets an empty array with 9 slots
     let _board = new Array(9);
@@ -35,7 +41,31 @@ const board = (() => {
     //initialize all the necessary functions related to the player object
     let _playerInit = () => {
         _getPlayerMove();
+        _markerBtns();
     };
+
+    //set the marker used by the player
+    let _markerSetter = ((marker) => {
+        player1.setMarker(marker);
+    });
+
+    let _markerBtns = (() => {
+        //start with the x button focused
+        _xbuttonMarker.classList = "clicked-button";
+
+        //add click event to the x button
+        _xbuttonMarker.addEventListener("click", () => {
+            _markerSetter("x");
+            _obuttonMarker.classList.remove("clicked-button");
+        });
+
+        //add click event to the o button
+        _obuttonMarker.addEventListener("click", () => {
+            _markerSetter("o");
+            _xbuttonMarker.classList.remove("clicked-button");
+            _obuttonMarker.classList = "clicked-button";
+        });
+    });
 
     //insert the selected marker in the equivalent array index
     let _pushToBoard = ((idx) => {
@@ -46,7 +76,7 @@ const board = (() => {
         };
     });
 
-    //creates the visual representation for the choices
+    //render the visual representation for the choices
     let _renderPlayerChoice = (selected) => {
         let move = document.createElement("div");
         move.classList = player1.getMarker();
@@ -56,7 +86,7 @@ const board = (() => {
         };
     };
 
-    //return a string with the indexes containing the especified marker
+    //return a string with the indexes containing the specified marker
     let _reduceIdx = ((marker) => {
         const indexes = _board.reduce((idx, item, i) => {
             if (item === marker) {
@@ -71,14 +101,15 @@ const board = (() => {
     let _getPlayerMove = (() => {
         for (i = 0; i < _displayBoard.length; i++) {
             _displayBoard[i].addEventListener("click", e => {
-
+                //get the position form the id of the button clicked
                 let selectedCell = e.target.id.slice(-1);
                 _pushToBoard(selectedCell);//store the selected move in the array
                 _renderPlayerChoice(selectedCell);//display the selected move in the board
 
                 let xMoves = _reduceIdx("x").indexes;
-                console.log(xMoves);
+                let oMoves = _reduceIdx("o").indexes;
                 console.log(game.checkWin(xMoves));
+                console.log(game.checkWin(oMoves));
             });
         };
     });
